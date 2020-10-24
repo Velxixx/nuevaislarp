f56814d016063606fe02ed54b444b22e0a362d94
Lone Wolf <51468104+DarkWolfHD@users.noreply.github.com>
2020-10-24 04:13:21 -0400

    Add files via upload

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports.options = {
+    token: 'NzY5NDQ0MjE5MTEwMjkzNTI1.X5PGxQ.479UxwcVMmlBoru4gnibpDEUZ0c',
+    topicChannels: [
+      'status'
+    ],
+    roles: ["Admin"],
+    servers: [
+        { name: 'status', url: 'http://144.217.11.184:30106', alias: 'NuevaIsla' }
+    ],
+    pollRate: 30,
+};
